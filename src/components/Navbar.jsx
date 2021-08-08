e23e281a69771c3f41205c798cc466d67bc6eed3
tran duc huy <huysusu125@gmail.com>
2021-08-08 19:59:51 +0700

    start

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h1>My Hook App</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/components/Todos.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const Todos = () => {
+    const [Todos, setTodos] = useState([])
+    return (
+        <div className="todo-list">
+            
+        </div>
+    )
+}
+
+export default Todos;
